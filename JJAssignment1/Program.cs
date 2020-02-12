961df00799896a0360f0745b5c9a8ef6f882dddb
jujeonghwan <jujeonghwan@gmail.com>
2020-02-12 00:36:13 -0500

    initial commit of solution folder

---
+++ JJAssignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+ * Program.cs
+ * Assignment1
+ * 
+ *  Revision History
+ *      Jeonghwan Ju, 2020.02.11: Created
+ */
+using System;
+
+namespace JJAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
